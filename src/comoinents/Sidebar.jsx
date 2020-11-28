b41c57a42bcf2c6c944010172e1fc7bd014de434
dioshoc <65164704+dioshoc@users.noreply.github.com>
2020-11-28 18:06:20 +0300

    maxter

---
+++ src/comoinents/Sidebar.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="header">
+            <img src="https://www.flaticon.com/svg/static/icons/svg/3802/3802071.svg" alt="" />
+        </header>
+    );
+}
+
+export default Header;
